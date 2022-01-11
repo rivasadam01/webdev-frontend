6230e8c305b1e6ccc4ea63f6137d48a090edfee8
rivasadam01 <rivasadam01@gmail.com>
2022-01-10 22:58:12 -0800

    Started to add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {loginUser,getUser,getUserToken} from './store/entities/user'
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login'
 import './App.css';
 
 function App() {
-  const dispatch=useDispatch();
-  const userToken=useSelector(getUserToken);
-  const user=useSelector(getUser);
-
-  useEffect(()=>{
-    dispatch(loginUser({username:'zero',password:'password'}))
-  },[])
-
   return (
     <div className="App">
       <h1>Webdev</h1>
-      {user.nickname}, token:{userToken}
+
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
