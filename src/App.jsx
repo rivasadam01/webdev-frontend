6939418d5f644890ac1849ed133b1924bcbf2403
rivasadam01 <rivasadam01@gmail.com>
2022-01-10 22:47:02 -0800

    Testing selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
-import {useDispatch} from 'react-redux';
-import {loginUser} from './store/entities/user'
+import {useDispatch, useSelector} from 'react-redux';
+import {loginUser,getUser,getUserToken} from './store/entities/user'
 import './App.css';
 
 function App() {
   const dispatch=useDispatch();
+  const userToken=useSelector(getUserToken);
+  const user=useSelector(getUser);
+
   useEffect(()=>{
     dispatch(loginUser({username:'zero',password:'password'}))
   },[])
+
   return (
     <div className="App">
       <h1>Webdev</h1>
-      
+      {user.nickname}, token:{userToken}
     </div>
   );
 }
