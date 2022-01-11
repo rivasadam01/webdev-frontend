2f507c2cea9e64349205cf6fd6c9ad8dbe2c6461
rivasadam01 <rivasadam01@gmail.com>
2022-01-10 22:30:54 -0800

    Testing redux api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import {loginUser} from './store/entities/user'
 import './App.css';
 
 function App() {
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(loginUser({username:'zero',password:'password'}))
+  },[])
   return (
     <div className="App">
       <h1>Webdev</h1>
+      
     </div>
   );
 }
