1aca7154239c3f49f95b47bf5b31d0175b2fed20
rivasadam01 <rivasadam01@gmail.com>
2022-01-10 22:29:50 -0800

    Created the api middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
 import { configureStore,getDefaultMiddleware } from "@reduxjs/toolkit";
 import rootReducer from './rootReducer';
+import apiMiddleWare from './middleware/api';
 
 const store=()=>configureStore({
     reducer: rootReducer,
-    middleware:[...getDefaultMiddleware()]
+    middleware:[...getDefaultMiddleware(),apiMiddleWare]
 })
 
 export default store;
---
+++ src/store/middleware/api.js
@@ -0,0 +1,25 @@
+import http from '../services/http'
+import * as apiActions from "../actions/api"
+
+const api=({dispatch})=>next=>async action=>{
+    if(action.type!==apiActions.apiCallBegan.type)return next(action);
+    const {url,method,data,onStart,onSuccess,onError}=action.payload;
+
+    dispatch({type:onStart})
+    next(action);
+    try{
+        const response=await http.request({
+            url,
+            method,
+            data
+        });
+        dispatch(apiActions.apiCallSuccess(response.data));
+        if(onSuccess)dispatch({type:onSuccess,payload:response.data});
+    }
+    catch(err){
+        dispatch(apiActions.apiCallFailed(err.message));
+        if(onError)dispatch({type:onError,payload:err.message});
+    }
+}
+
+export default api;
