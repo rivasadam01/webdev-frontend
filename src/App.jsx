2244eb42be1c6256cdbd170c76ae0af771d3b9d1
rivasadam01 <rivasadam01@gmail.com>
2022-01-12 11:24:08 -0800

    added protected route and lout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/auth/Login'
+import Protected from './components/routes/Protected';
+import Home from './components/Home';
+import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
 import './App.scss';
 
 function App() {
@@ -7,6 +10,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/logout" element={<Logout/>}/>
+        <Route path="/" element={<Protected/>}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
       </Routes>
     </div>
   );
