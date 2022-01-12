59538cae6250d3e73b9a86d61934fe40eee22ca0
rivasadam01 <rivasadam01@gmail.com>
2022-01-12 11:23:38 -0800

    added persistance and errors

--- src/store/entities/user.js
+++ src/store/entities/user.js
@@ -3,30 +3,46 @@ import { createSelector } from "reselect";
 import * as apiActions from '../actions/api';
 
 //temporary variables..should be in environment?
-let url='/auth';
+const url='/auth';
+const userStorageKey='user';
+const tokenStorageKey='key';
 
 const slice=createSlice({
     name:'user',
     initialState:{
-        user:{},
-        token:null,
-        isLoggedIn:false,
+        user:JSON.parse(localStorage.getItem(userStorageKey)) || {},
+        token:localStorage.getItem(tokenStorageKey) || null,
+        isLoggedIn:!!localStorage.getItem(tokenStorageKey),
+        errors:null
     },
     reducers:{
-        userLoginRequested:(user,action)=>{},
+        userLoginRequested:(user,action)=>{
+            user.errors=null;
+        },
         userLoggedIn:(user,action)=>{
             user.user=action.payload.user;
             user.token=action.payload.token;
             user.isLoggedIn=true;
+            localStorage.setItem(userStorageKey,JSON.stringify(user.user));
+            localStorage.setItem(tokenStorageKey,user.token);
         },
-        userLoginFailed:(user,action)=>{}
+        userLoginFailed:(user,action)=>{
+            user.errors=action.payload;
+        },
+        userLoggedOut:(user,action)=>{
+            user.user=null;
+            user.token=null;
+            user.isLoggedIn=false;
+            localStorage.removeItem(userStorageKey);
+            localStorage.removeItem(tokenStorageKey);
+        }
     }
 });
 
 export default slice.reducer;
 
 //user actions
-const {userLoginRequested,userLoggedIn,userLoginFailed}=slice.actions;
+const {userLoginRequested,userLoggedIn,userLoginFailed,userLoggedOut}=slice.actions;
 
 //commands
 export const loginUser=(user)=>dispatch=>
@@ -38,6 +54,8 @@ export const loginUser=(user)=>dispatch=>
     onSuccess:userLoggedIn.type,
     onError:userLoginFailed.type
 }));
+
+export const logoutUser=()=>userLoggedOut();
 
 
 //selectors
@@ -55,4 +73,9 @@ export const getUserToken=createSelector(
 export const isLoggedIn=createSelector(
     state=>state.entities.user.isLoggedIn,
     isLoggedIn=>isLoggedIn
+)
+
+export const getUserErrors=createSelector(
+    state=>state.entities.user,
+    user=>user.errors
 )
